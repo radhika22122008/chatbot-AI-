43cc0ae3f52ab7825afc4c196317224297ec6ac7
radhika22122008 <radhikawalia22122008@gmail.com>
2026-06-26 03:32:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 import google.generativeai as genai
 
-genai.configure(api_key="")
+genai.configure(api_key="AQ.Ab8RN6Js_Jb6qJyS_7eLYjiSZA8GVNXqiz0du7rTt0MKMYJ-lw")
 
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
